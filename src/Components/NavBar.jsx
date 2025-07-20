beb3336914012da77929f3328c3a36a61c731297
name <adekunlemichael198@gmail.com>
2025-07-20 20:36:42 +0100

    Recovered from index corruption

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -66,13 +66,13 @@ const NavBar = () => {
 
                 <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
                     <div className="fixed inset-0 z-50" />
-                    <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                        <div className="flex items-center justify-between">
+                        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-blue-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                            <div className="flex items-center justify-between">
                                 <a href="#" className="flex items-center -m-1.5 p-1.5">
                                 <img
                                     alt="HomeLogo"
                                     src={WhiteLogo}
-                                    className="h-10 w-auto "
+                                    className="h-10 w-auto"
                                 />
                                     <span className="text-3xl font-bold text-white">Futuride</span>
                                 </a>
@@ -80,29 +80,40 @@ const NavBar = () => {
                                 <button
                                     type="button"
                                     onClick={() => setMobileMenuOpen(false)}
-                                    className="-m-2.5 rounded-md p-2.5 text-white">
+                                    className="-m-2.5 bg-gray-300 rounded-md p-2.5 text-white">
 
                                     <span className="sr-only">Close menu</span>
                                     <XMarkIcon aria-hidden="true" className="size-6" />
                                 </button>
-                        </div>
+                            </div>
 
-                        <div className="mt-6 flow-root">
-                            <div className="-my-6 divide-y divide-gray-500/10">
-                                <div className="space-y-2 py-6">
-                                {navigation.map((item) => (
-                                    <a
-                                    key={item.name}
-                                    href={item.href}
-                                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:text-black hover:bg-gray-50"
-                                    >
-                                    {item.name}
-                                    </a>
-                                ))}
+                            <div className="mt-6 flow-root">
+                                <div className="-my-6 divide-y divide-gray-500/10">
+                                    <div className="space-y-2 py-6">
+                                        {navigation.map((item) =>
+                                            item.onClick ? (
+                                                <button key={item.name} onClick={() => {
+                                                    item.onClick(); // scroll to section
+                                                    setMobileMenuOpen(false); // close mobile menu
+                                                    }}
+                                                    className="w-full text-left block rounded-lg px-3 py-2 text-base font-semibold text-white hover:text-black hover:bg-gray-50">
+                                                    {item.name}
+                                                </button>
+                                                ) : (
+                                                <a
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    onClick={() => setMobileMenuOpen(false)} // still close menu
+                                                    className="block rounded-lg px-3 py-2 text-base font-semibold text-white hover:text-black hover:bg-gray-50"
+                                                >
+                                                    {item.name}
+                                                </a>
+                                        )
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </DialogPanel>
+                        </DialogPanel>
                 </Dialog>
             </header>
         </div>
